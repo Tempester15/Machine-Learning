3ffee17d8f7cf29b02bd421f72d44cbfb6e98aa9
Tempester15 <egotheliving@gmail.com>
2026-03-19 12:48:57 +0530

    add Beautiful Soup Reusable code file

--- Data_Extraction/PythonAPIs.py
+++ Data_Extraction/PythonAPIs.py
@@ -1,2 +1,16 @@
 import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+
+page_number = 2
+url = f"https://anisuge.se/status/finished-airing?page={page_number}"
+
+
+page = requests.get(url)
+html_content = page.text.encode("utf=8")
+soup = BeautifulSoup(html_content)
+#flex grid = 
+anime_titles = soup.find('div', class_ = "original anime main-card")
+print(anime_titles.prettify())
+
+# print(soup.prettify())
